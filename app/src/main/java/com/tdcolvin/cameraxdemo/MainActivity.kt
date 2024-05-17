c16004f257e9b9d92fba50a95923eefb2631bcd0
Tom Colvin <tdcolvin@gmail.com>
2024-05-17 12:27:08 +0100

    Simplified CameraAppScreen()

--- app/src/main/java/com/tdcolvin/cameraxdemo/MainActivity.kt
+++ app/src/main/java/com/tdcolvin/cameraxdemo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tdcolvin.cameraxdemo
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,8 +8,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.tdcolvin.cameraxdemo.ui.navigation.MainNavigation
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.tdcolvin.cameraxdemo.ui.permission.WithPermission
 import com.tdcolvin.cameraxdemo.ui.theme.CameraXDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,9 +22,34 @@ class MainActivity : ComponentActivity() {
         setContent {
             CameraXDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                     MainNavigation(modifier = Modifier.padding(innerPadding))
+                    WithPermission(
+                        modifier = Modifier.padding(innerPadding),
+                        permission = Manifest.permission.CAMERA
+                    ) {
+                        CameraAppScreen()
+                    }
                 }
             }
         }
     }
 }
+
+@Composable
+fun CameraAppScreen() {
+    //Start here!
+}
+
+@Preview(widthDp = 400, heightDp = 700, showSystemUi = true)
+@Composable
+fun PreviewCameraApp() {
+    CameraXDemoTheme {
+        CameraAppScreen()
+    }
+}
+
+fun Color.Companion.random(): Color =
+    Color(
+        red = Math.random().toFloat(),
+        green = Math.random().toFloat(),
+        blue = Math.random().toFloat()
+    )
