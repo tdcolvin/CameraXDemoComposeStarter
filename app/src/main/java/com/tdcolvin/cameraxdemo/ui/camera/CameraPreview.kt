686424152350ccfcdde260ae789808dd0bdc39fd
Tom Colvin <tdcolvin@gmail.com>
2024-12-06 22:36:13 +0000

    Example completed CameraPreview composable

---
+++ app/src/main/java/com/tdcolvin/cameraxdemo/ui/camera/CameraPreview.kt
@@ -0,0 +1,75 @@
+package com.tdcolvin.cameraxdemo.ui.camera
+
+import androidx.camera.core.CameraControl
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+
+@Composable
+fun MyCameraPreview(
+    modifier: Modifier = Modifier,
+    lensFacing: Int,
+    zoomLevel: Float,
+    imageCaptureUseCase: ImageCapture
+) {
+    val previewUseCase = remember { androidx.camera.core.Preview.Builder().build() }
+
+    var cameraProvider by remember { mutableStateOf<ProcessCameraProvider?>(null) }
+    var cameraControl by remember { mutableStateOf<CameraControl?>(null) }
+
+    val localContext = LocalContext.current
+
+    fun rebindCameraProvider() {
+        cameraProvider?.let { cameraProvider ->
+            val cameraSelector = CameraSelector.Builder()
+                .requireLensFacing(lensFacing)
+                .build()
+            cameraProvider.unbindAll()
+            val camera = cameraProvider.bindToLifecycle(
+                localContext as LifecycleOwner,
+                cameraSelector,
+                previewUseCase, imageCaptureUseCase
+            )
+            cameraControl = camera.cameraControl
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        val providerFuture = ProcessCameraProvider.getInstance(localContext)
+        providerFuture.addListener({
+            cameraProvider = providerFuture.get()
+            rebindCameraProvider()
+        }, ContextCompat.getMainExecutor(localContext))
+    }
+
+    LaunchedEffect(lensFacing) {
+        rebindCameraProvider()
+    }
+
+    LaunchedEffect(zoomLevel) {
+        cameraControl?.setLinearZoom(zoomLevel)
+    }
+
+    AndroidView(
+        modifier = modifier.fillMaxSize(),
+        factory = { context ->
+            PreviewView(context).also {
+                previewUseCase.setSurfaceProvider(it.surfaceProvider)
+                rebindCameraProvider()
+            }
+        }
+    )
+}
